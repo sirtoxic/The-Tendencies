0294def3de8355167b76f569917a797549e6521d
SirToxic <leerussell1991@gmail.com>
2016-07-11 02:22:07 +1000

    added nav links

--- admin/createuser.php
+++ admin/createuser.php
@@ -80,6 +80,12 @@ if (!empty($_POST['email'])&& !empty($_POST['password'])) {
                     <li>
                         <a href="../admin/deleteuser.php"><i class="fa fa-user-times"></i> Delete User</a>
                     </li>
+                    <li>
+                        <a href="../admin/createpost.php"><i class="fa fa-plus-square-o"></i> Create Post</a>
+                    </li>
+                    <li>
+                        <a href="../admin/editpost.php"><i class="fa fa-pencil-square-o"></i> Edit Post</a>
+                    </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
