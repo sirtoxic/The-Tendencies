fc7c906bdce531a824bfcf38fdfa0d6d9d8025e0
SirToxic <leerussell1991@gmail.com>
2016-07-19 12:45:58 +1000

    added locic to retreve a list of posts to be displayed on the edit post screen. also changed the sql logic to retreve posts in desending time order this has created an off by 1 error in post retreving

--- scripts/postLogic.php
+++ scripts/postLogic.php
@@ -4,7 +4,7 @@ require 'db.php';
 
 function displayPosts(){
 	require 'db.php';
-	$records = $conn->prepare('SELECT ALL * FROM `posts` WHERE `p_visable` = 1');
+	$records = $conn->prepare('SELECT ALL * FROM `posts` WHERE `p_visable` = 1 ORDER BY `p_date`DESC');
     $records->execute();
     $results = $records->fetch(PDO::FETCH_ASSOC);
 
@@ -28,5 +28,47 @@ function displayPosts(){
 	}
 }
 
+
+
+function displayPostEdit(){
+	require 'db.php';
+	$records = $conn->prepare('SELECT ALL * FROM `posts` WHERE 1 ORDER BY `p_date`DESC');
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    echo 	"<table class=\"table table-striped\">
+            	<thead>
+              		<tr>
+                		<th>ID</th>
+                		<th>Title</th>
+                		<th>Content</th>
+                		<th>Creator</th>
+                		<th>Date</th>
+                		<th>Visable</th>
+              		</tr>
+            	</thead>
+            <tbody>";
+  				
+
+	while($result = $records->fetch(PDO::FETCH_ASSOC)) {
+		$postID = $result['p_ID'];	
+		$postCreator = $result['p_creator'];
+		$postDate = $result['p_date'];	
+		$postTitle = $result['p_title'];
+		$postContent = $result['p_content'];
+		$postVisable = $result['p_visable'];
+
+	    echo 	"<tr>
+                	<td>".$result['p_ID']."</td>
+                	<td>".$result['p_title']."</td>
+                	<td>".$result['p_content']."</td>
+                	<td>".$result['p_creator']."</td>
+                	<td>".$result['p_date']."</td>
+                	<td>".$result['p_visable']."</td>
+              	</tr>";
+    }
+    echo "</tbody>
+          </table>";
+}
 
 ?>
